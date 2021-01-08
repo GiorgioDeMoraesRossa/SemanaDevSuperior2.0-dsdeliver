7725811afbfaf3983bd7c12926315382d4d6fa72
Giorgio Rossa <giorgiorossa@inf.ufpel.edu.br>
2021-01-08 11:40:09 -0200

    Finish front web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,18 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import React from "react";
 import "./App.css";
 
 import Routes from "./routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 
 import { Product } from "./types";
+import { formatPrice } from "./helpers";
 
 type Props = {
   product: Product;
+  onSelectProduct: (product: Product) => void;
+  isSelected: boolean;
 };
 
-function formatPrice(price: number) {
-  const formatter = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  return formatter.format(price);
-}
-
-export default function ProductCard({ product }: Props) {
+export default function ProductCard({
+  product,
+  onSelectProduct,
+  isSelected,
+}: Props) {
   return (
-    <div className="orders-card-container">
+    <div
+      className={`order-card-container ${isSelected ? "selected" : ""}`}
+      onClick={() => onSelectProduct(product)}
+    >
       <h3 className="order-card-title">{product.name}</h3>
       <img
         alt={product.name}
