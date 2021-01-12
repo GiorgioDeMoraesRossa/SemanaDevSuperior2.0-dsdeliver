943bb4042e09d8089f56adf0659a9dac90ee0bdb
Giorgio Rossa <giorgiorossa@inf.ufpel.edu.br>
2021-01-12 18:38:07 -0200

    mobile pronto

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,18 +1,55 @@
-import React from "react";
-import { StyleSheet, ScrollView, Text, Image, View } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, ScrollView, Text, Image, View, Alert } from "react-native";
+import {
+  RectButton,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
 import Header from "../Header";
 import OrderCard from "../OrderCard";
+import { fetchOrders } from "../api";
+import { Order } from "../types";
+import { useNavigation, useIsFocused } from "@react-navigation/native";
 
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+      .then((response) => setOrders(response.data))
+      .catch(() => Alert.alert("Houveu um erro ao buscar os pedidos"))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [isFocused]);
+
+  const handleOnPress = (order: Order) => {
+    navigation.navigate("OrderDetails", {
+      order,
+    });
+  };
+
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoading ? (
+          <Text>Buscando pedidos...</Text>
+        ) : (
+          orders.map((order) => (
+            <TouchableWithoutFeedback
+              key={order.id}
+              onPress={() => handleOnPress(order)}
+            >
+              <OrderCard order={order} />
+            </TouchableWithoutFeedback>
+          ))
+        )}
       </ScrollView>
     </>
   );
